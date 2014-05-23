5b469c105b8358d68557ddd2282a6f1882cf7b0b
Giles Hall <giles@polymerase.org>
2014-05-23 18:34:26 -0400

    checkpoint

--- src/api.py
+++ src/api.py
@@ -4,21 +4,29 @@ import logging
 
 __all__ = [
     "Cube",
+    "Union",
+    "Cylinder",
 ]
 
 logger = logging.getLogger(__name__)
 
-class Cube(SCAD_Object):
-    SCAD_Template = "cube(%(size)s, center=%(center)s)"
+class Union(SCAD_Primitive):
+    SCAD_Name = "union"
+
+class Cube(SCAD_Primitive):
+    SCAD_Name = "cube"
     Aliases = {
         'x': 'size.x',
         'y': 'size.y',
         'z': 'size.z',
     }
     Defaults = {
-        "size": 1,
-        "center": False
+        "size": {"type": Vector3D, "default": lambda: Vector3D(1.0, 1.0, 1.0)},
+        "center": {"type": bool, "default": False},
     }
+
+    def get_scad_args(self):
+        return [self.size, ("center", self.center)]
 
     def get_size(self):
         return self["size"]
@@ -30,5 +38,83 @@ class Cube(SCAD_Object):
     size = property(get_size, set_size)
 
     def process_args(self, args):
-        size = args[0] if (len(args) == 1) else args
-        return {"size": size}
+        ret = {}
+        if args:
+            ret["size"] = args[0] if (len(args) == 1) else args
+        return ret
+
+class RadialResolution(SCAD_Primitive):
+    Defaults = {
+        "fn": {"type": float, "default": None},
+        "fs": {"type": float, "default": None},
+        "fa": {"type": float, "default": None},
+    }
+
+    def get_scad_args(self):
+        ret = []
+        if self.fn:
+            ret.append(("$fn", self.fn))
+        else:
+            if self.fa:
+                ret.append(("$fa", self.fa))
+            if self.fs:
+                ret.append(("$fs", self.fs))
+        return ret
+
+class Cylinder(SCAD_Primitive):
+    SCAD_Name = "cylinder"
+    Aliases = {
+        'fn': 'resolution.fn',
+        'fa': 'resolution.fa',
+        'fs': 'resolution.fs',
+        'radius': 'radius_1',
+        'r': 'radius_1',
+        'r1': 'radius_1',
+        'r2': 'radius_2',
+        'diameter': 'diameter_1',
+        'd': 'diameter_1',
+        'd1': 'diameter_1',
+        'd2': 'diameter_2',
+        'h': 'height',
+    }
+    Defaults = {
+        "radius_1": {"type": float, "default": 1.0},
+        "radius_2": {"type": float, "default": None},
+        "center": {"type": bool, "default": False},
+        "resolution": {"type": RadialResolution, "default": lambda: RadialResolution()},
+        "height": {"type": float, "default": 1.0},
+    }
+
+    def get_scad_args(self):
+        scad = []
+        if not self.r2:
+            scad.append(("r", self.radius_1))
+        else:
+            scad.append(("r1", self.radius_1))
+            scad.append(("r2", self.radius_2))
+        scad.append(("h", self.height))
+        scad.append(("center", self.center))
+        scad += self.resolution.get_scad_args()
+        return scad
+
+    def get_diamater_1(self):
+        return self.radius_1 * 2
+    def set_diamater_1(self, dia):
+        self.radius_1 = dia / 2.0
+    diameter_1 = property(get_diamater_1, set_diamater_1)
+
+    def get_diamater_2(self):
+        return self.radius_2 * 2
+    def set_diamater_2(self, dia):
+        self.radius_2 = dia / 2.0
+    diameter_2 = property(get_diamater_2, set_diamater_2)
+
+    def get_resolution(self):
+        if self["resolution"] == None:
+            self["resolution"] = RadialResolution()
+        return self["resolution"]
+    
+    def set_resolution(self, val):
+        self["resolution"] = val
+    resolution = property(get_resolution, set_resolution)
+
--- test.py
+++ test.py
@@ -1,9 +1,62 @@
 import unittest
 from scad import *
+from scad.base import BaseObject
 
 def check_vector(vector, **answers):
     for (key, value) in answers.items():
         assert getattr(vector, key) == value
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        self.parent_1 = BaseObject("parent_1")
+        self.parent_2 = BaseObject("parent_2")
+        self.child_1 = BaseObject("child_1")
+        self.child_2 = BaseObject("child_2")
+        self.child_3 = BaseObject("child_3")
+        self.child_4 = BaseObject("child_4")
+        self.child_5 = BaseObject("child_5")
+        self.child_6 = BaseObject("child_6")
+        self.parent_1.children = (self.child_1, self.child_2, self.child_3)
+        self.parent_2.children = (self.child_4, self.child_5, self.child_6)
+
+    def test_parent_child(self):
+        self.assertIn(self.child_1, self.parent_1.children)
+        self.assertIn(self.child_2, self.parent_1.children)
+        self.assertNotIn(self.child_4, self.parent_1.children)
+        self.assertEquals(self.child_1.parent, self.parent_1)
+        self.assertNotEquals(self.child_4.parent, self.parent_1)
+        self.parent_2.add_child(self.child_1)
+        self.assertNotIn(self.child_1, self.parent_1.children)
+        self.assertIn(self.child_1, self.parent_2.children)
+        self.assertEquals(self.child_1.parent, self.parent_2)
+
+class TestRadialResolution(unittest.TestCase):
+    def test_cylinder_radial_resolution_scad(self):
+        c = Cylinder(h=10, r=20, fn=10, fa=1)
+        answer = "cylinder(r=20.0, h=10.0, center=false, $fn=10.0);"
+        self.assertEquals(c.render_scad().strip(), answer)
+        c.fn = 0
+        answer = "cylinder(r=20.0, h=10.0, center=false, $fa=1.0);"
+        self.assertEquals(c.render_scad().strip(), answer)
+        c.fs = 2
+        answer = "cylinder(r=20.0, h=10.0, center=false, $fa=1.0, $fs=2.0);"
+        self.assertEquals(c.render_scad().strip(), answer)
+        c.fs = 0
+        c.fa = 0
+        answer = "cylinder(r=20.0, h=10.0, center=false);"
+        self.assertEquals(c.render_scad().strip(), answer)
+
+class TestCylinder(unittest.TestCase):
+    def test_sphere_creation(self):
+        c = Cylinder(h=10, r=20)
+        self.assertEquals(c.h, 10)
+        self.assertEquals(c.r, 20)
+        self.assertEquals(c.d1, 40)
+
+    def test_sphere_scad(self):
+        c = Cylinder(h=10, r=20)
+        answer = "cylinder(r=20.0, h=10.0, center=false);"
+        self.assertEquals(c.render_scad().strip(), answer)
 
 class TestCube(unittest.TestCase):
     def test_cube_creation(self):
@@ -20,26 +73,26 @@ class TestCube(unittest.TestCase):
         check_vector(c, **answers)
 
     def test_cube_scad(self):
-        answer = "cube([1, 2, 3], center=false)"
+        answer = "cube([1.0, 2.0, 3.0], center=false);"
         c = Cube([1,2,3])
         scad = c.render_scad()
         self.assertEquals(scad.strip(), answer)
 
 class TestVector3D(unittest.TestCase):
     def test_vector_creation(self):
-        answers = {'x': 1, 'y': 2, 'z': 3}
+        answers = {'x': 1.0, 'y': 2.0, 'z': 3.0}
         v1 = Vector3D(1, 2, 3)
         check_vector(v1, **answers)
         v1 = Vector3D([1, 2, 3])
         check_vector(v1, **answers)
 
     def test_vector_aliases(self):
-        answers = {'X': 1, 'Y': 2, 'Z': 3}
+        answers = {'X': 1.0, 'Y': 2.0, 'Z': 3.0}
         v1 = Vector3D(1, 2, 3)
         check_vector(v1, **answers)
 
     def test_vector_subtraction(self):
-        answers = {'x': 1, 'y': -1, 'z': 2}
+        answers = {'x': 1.0, 'y': -1.0, 'z': 2.0}
         v1 = Vector3D(2, 1, 5)
         v2 = Vector3D(1, 2, 3)
         check_vector(v1 - v2, **answers)
@@ -50,10 +103,21 @@ class TestVector3D(unittest.TestCase):
         self.assertEquals(v1.dotproduct(v2), 122)
 
     def test_vector_cross_product(self):
-        answers = {'x': -3, 'y': 6, 'z': -3}
+        answers = {'x': -3.0, 'y': 6.0, 'z': -3.0}
         v1 = Vector3D(2, 3, 4)
         v2 = Vector3D(5, 6, 7)
         check_vector(v1.crossproduct(v2), **answers)
+
+class TestUnion(unittest.TestCase):
+    def test_union_creation(self):
+        u = Union()
+        u = Union()(Cube())
+        u = Union()(Cube(), Cube())
+
+    def test_union_scad(self):
+        u = Union()(Cube(), Cube(2))
+        answer = 'union() {\n    cube([1.0, 1.0, 1.0], center=false);\n    cube([2.0, 2.0, 2.0], center=false);\n}'
+        self.assertEquals(u.render_scad().strip(), answer)
 
 if __name__ == '__main__':
     unittest.main()
