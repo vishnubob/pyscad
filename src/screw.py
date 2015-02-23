43727feab5173de1c8d40e68ed2a263b272c0296
Giles Hall <giles@polymerase.org>
2015-02-22 19:07:22 -0500

    screw checkpoint

--- src/screw.py
+++ src/screw.py
@@ -33,53 +33,10 @@ class Helix(object):
             z = pitch_step * angle + zo
             yield self.apply_limits((x, y, z))
 
-class HelixStitcher(SCAD_Object):
-    def __init__(self, helix_list):
-        self.helix_list = helix_list
-
-    def scad(self):
-        points = []
-        for helix in self.helix_list:
-            points.extend(list(helix))
-        faces = []
-        helix = self.helix_list[0]
-        point_count = len(helix)
-        resolution = helix.resolution
-        for hidx in range(len(self.helix_list)):
-            offset = point_count * hidx
-            next_helix = (offset + point_count) % len(points)
-            previous_helix = (offset - point_count) % len(points)
-            for idx in range(point_count):
-                if hidx < (len(self.helix_list) - 1):
-                    if idx < (point_count - 1):
-                        face = (offset + idx, offset + idx + 1, next_helix + idx)
-                        faces.append(face)
-                else:
-                    if (idx + resolution) < point_count:
-                        face = (offset + idx, offset + idx + 1, next_helix + idx + resolution)
-                        faces.append(face)
-                if hidx == 0:
-                    if (idx - resolution) > 0:
-                        face = (offset + idx, offset + idx - 1, previous_helix + idx - resolution)
-                        faces.append(face)
-                elif idx > 0:
-                    face = (offset + idx, offset + idx - 1, previous_helix + idx)
-                    faces.append(face)
-        return Polyhedron(points=points, faces=faces)
-
-helix1 = Helix(10, 8, 40, 10)
-helix2 = Helix(15, 8, 40, 10, offset=(0, 0, 2))
-helix3 = Helix(13, 8, 40, 10, offset=(0, 0, 4))
-hlist = [helix1, helix2, helix3]
-hs = HelixStitcher(hlist)
-hs.render("helix.scad")
-
-class Thread(SCAD_Object):
-    major_diameter = 0
-    pitch = 0
-    length = 0
-    thread_angle = math.radians(60)
-    resolution = 100
+class ThreadProfile(object):
+    def __init__(self, major_diameter, pitch):
+        self.major_diameter = major_diameter
+        self.pitch = pitch
 
     @property
     def height(self):
@@ -105,32 +62,70 @@ class Thread(SCAD_Object):
     def slope_offset(self):
         return self.pitch * (3 / 16.0)
 
-    def thread_profile(self, offset=(0, 0, 0)):
+    def __iter__(self):
         z = 0
-        pt1 = (0, self.minor_diameter, z)
+        pt1 = (self.minor_diameter, z)
         z += self.valley_offset
-        pt2 = (0, self.minor_diameter, z)
+        pt2 = (self.minor_diameter, z)
         z += self.slope_offset
-        pt3 = (0, self.major_diameter, z)
+        pt3 = (self.major_diameter, z)
         z += self.crest_offset
-        pt4 = (0, self.major_diameter, z)
-        z += self.slope_offset
-        pt5 = (0, self.minor_diameter, z)
-        points = [pt1, pt2, pt3, pt4, pt5]
-        points = [(x + offset[0], y + offset[1], z + offset[2]) for (x, y, z) in points]
-        return points
+        pt4 = (self.major_diameter, z)
+        points = [pt1, pt2, pt3, pt4]
+        return iter(points)
+
+class Screw(SCAD_Object):
+    major_diameter = 6.35
+    pitch = 1.27
+    length = 20
+
+    def build_screw(self, helix_list):
+        points = []
+        for helix in helix_list:
+            points.extend(list(helix))
+        faces = []
+        helix = helix_list[0]
+        point_count = len(helix)
+        resolution = helix.resolution
+        for hidx in range(len(helix_list)):
+            offset = point_count * hidx
+            next_helix = (offset + point_count) % len(points)
+            previous_helix = (offset - point_count) % len(points)
+            for idx in range(point_count):
+                if hidx < (len(helix_list) - 1):
+                    if idx < (point_count - 1):
+                        face = (offset + idx, offset + idx + 1, next_helix + idx)
+                        faces.append(face)
+                else:
+                    if (idx + resolution) < point_count:
+                        face = (offset + idx, offset + idx + 1, next_helix + idx + resolution)
+                        faces.append(face)
+                if hidx == 0:
+                    if (idx - resolution) > 0:
+                        face = (offset + idx, offset + idx - 1, previous_helix + idx - resolution)
+                        faces.append(face)
+                elif idx > 0:
+                    face = (offset + idx, offset + idx - 1, previous_helix + idx)
+                    faces.append(face)
+        points.append((0, 0, self.length))
+        endpoint = len(points) - 1
+        for idx in range(resolution):
+            offset = len(helix_list) * point_count - resolution + idx
+            face = (offset - 1, offset, endpoint)
+            faces.append(face)
+        return Polyhedron(points=points, faces=faces)
 
     def scad(self):
-        points = self.thread_profile()
-        points += self.thread_profile((5, 0, 0))
-        faces = [
-            [0, 1, 5], [5, 6, 1],
-            [1, 2, 6], [6, 7, 2],
-            [2, 3, 7], [7, 8, 3],
-            [3, 4, 8], [8, 9, 4],
-        ]
-        p = Polyhedron(points=points, faces=faces)
-        return p
+        profile = ThreadProfile(self.major_diameter, self.pitch)
+        min_limit = (None, None, 0)
+        max_limit = (None, None, self.length)
+        length = self.length + self.pitch * 2
+        helix_list = []
+        for (radius, z_offset) in profile:
+            offset = (0, 0, z_offset - self.pitch)
+            helix = Helix(radius, self.pitch, length, offset=offset, min_limit=min_limit, max_limit=max_limit)
+            helix_list.append(helix)
+        return self.build_screw(helix_list)
 
-t = Thread(pitch=2, major_diameter=2)
-t.render("threads.scad")
+s = Screw()
+s.render("helix.scad")
