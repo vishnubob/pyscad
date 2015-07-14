d4f599fc6e225e9e9c1ba8e455732f9b875058f5
Giles Hall <giles@polymerase.org>
2015-07-14 13:22:27 -0400

    pypi updates

--- setup.py
+++ setup.py
@@ -3,15 +3,26 @@
 from setuptools import setup
 
 sctk = {
-    "name": "scad",
+    "name": "PySCAD",
     "description": "Python based OpenSCAD framework",
     "author":"Giles Hall",
+    "author_email": "giles@polymerase.org",
+    "keywords": ["scad", "3D modeling", "3D printing"],
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Multimedia :: Graphics :: 3D Rendering",
+    ],
     "packages": ["scad"],
     "package_dir": {"scad": "src"},
     "install_requires": [
         "pint", 
         "freetype-py"
     ],
+    "url": "https://github.com/vishnubob/pyscad",
+    "download_url": "https://github.com/vishnubob/pyscad/archive/v0.1.zip",
     "py_modules":[
         "scad.__init__", 
         "scad.base", 
